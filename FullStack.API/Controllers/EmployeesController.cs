6cd3c2507bbd5a6873eb66a35ab444fd921b05b9
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-07-25 02:43:18 +0500

    FullStack.API

---
+++ FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStack.API.Data;
+using FullStack.API.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/values
+        [HttpGet("get-all-employees")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost("add-employee")]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _context.Employees.AddAsync(employeeRequest);
+            await _context.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+
+        // GET: api/employee/id
+        [HttpGet("get-employee-by-id/{id}")]
+        public async Task<IActionResult> GetEmployeeById(Guid id)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(em => em.Id==id);
+            if (employee == null)
+                return NotFound();
+            return Ok(employee);
+        }
+
+        [HttpPut("update-employee-by-id/{id}")]
+        public async Task<IActionResult> GetEmployeeById(Guid id,[FromBody] Employee updateEmployee)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateEmployee.Name;
+            employee.Email = updateEmployee.Email;
+            employee.Department = updateEmployee.Department;
+            employee.Salary = updateEmployee.Salary;
+            employee.Phone = updateEmployee.Phone;
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete("delete-employee-by-id/{id}")]
+        public async Task<IActionResult> DeleteEmployeeById(Guid id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+                return NotFound();
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+
+    }
+}
+
---
+++ FullStack.API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FullStack.API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
